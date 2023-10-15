9b2ed70a691e45a32708ef028d8386e403c2eaaa
Andréas B <andreas.blondeau@gmail.com>
2023-10-15 17:30:07 +0200

    chore: installed tailwind css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['nuxt-lodash'],
+  modules: ["nuxt-lodash", "@nuxtjs/tailwindcss"],
 
   runtimeConfig: {
     OPENAI_API_KEY: process.env.OPENAI_API_KEY,
     PINECONE_INDEX_NAME: process.env.PINECONE_INDEX_NAME,
     PINECONE_API_KEY: process.env.PINECONE_API_KEY,
-    PINECONE_ENVIRONMENT: process.env.PINECONE_ENVIRONMENT
+    PINECONE_ENVIRONMENT: process.env.PINECONE_ENVIRONMENT,
   },
 
   lodash: {
-    prefix: '_',
-    upperAfterPrefix: false
-  }
-})
+    prefix: "_",
+    upperAfterPrefix: false,
+  },
+
+  components: [
+    {
+      path: "~/components/ui",
+      extensions: [".vue"],
+      prefix: "Ui",
+    },
+  ],
+});
